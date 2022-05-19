48a213ec0fbb3c17209a67fac481f384f79dcc5a
pkman92 <97434874+pkman92@users.noreply.github.com>
2022-05-19 13:28:05 +0300

    Create main.go

---
+++ internal/users/main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+// @version         1.0
+// @metadata.name   User Service
+// @metadata.description    This is the user service
+// @spec.owner    Team 1
+
+func main(){
+  fmt.Println("Hey I am a service, I need to be tracked, gettign started")
+}
